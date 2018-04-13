c8cfa082e892ea1874a05e88be0a651ac913d7f1
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2018-04-13 11:44:18 +0200

    Basic compose support

---
+++ stop.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+set -e
+
+export repo_pre=
+export tag=
+
+docker-compose stop
+docker-compose rm
---
+++ up.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+set -e
+
+export repo_pre=vt-optimus-solr02:5000/
+export tag=:1.0.0
+
+docker-compose up -d
