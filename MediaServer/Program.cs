ca0c38fac91ff7a816f759f7b65c7a7237ac9c84
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2018-04-23 08:08:46 +0200

    Added Application Insights

--- MediaServer/Program.cs
+++ MediaServer/Program.cs
@@ -19,6 +19,7 @@ namespace MediaServer
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseUrls("http://*:5000")
                 .UseStartup<Startup>()
                 .Build();
