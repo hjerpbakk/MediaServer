ef4aeea2dc878e2c0a26016cd7775f63aeec4434
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2018-04-24 07:50:43 +0200

    Added MemoryCache package

--- MediaServer/Startup.cs
+++ MediaServer/Startup.cs
@@ -34,6 +34,7 @@ namespace MediaServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMemoryCache();
             services.AddResponseCaching();
             services.AddMvc();
 
