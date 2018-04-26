a9e2b98c36e8a5d2e6882d675bda10733aff4936
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2018-04-26 14:14:14 +0200

    Trying another way to get images on Slack

--- MediaServer/Services/SlackService.cs
+++ MediaServer/Services/SlackService.cs
@@ -7,41 +7,42 @@ using SlackConnector.Models;
 
 namespace MediaServer.Services
 {
-	public class SlackService : ISlackService
+    public class SlackService : ISlackService
     {
         const string TestSlackChannel = "G73TGULC9";
 
-		readonly ISlackConnector connector;
-		readonly ISlackConfig config;
+        readonly ISlackConnector connector;
+        readonly ISlackConfig config;
         
-		public SlackService(ISlackConnector connector, ISlackConfig config)
+        public SlackService(ISlackConnector connector, ISlackConfig config)
         {
-			this.connector = connector;
-			this.config = config;
+            this.connector = connector;
+            this.config = config;
         }
 
-		public async Task PostTalkToChannel(Conference conference, Talk talk, string talkUrl, string thumbnailUrl)
-		{
-			var connection = await connector.Connect(config.SlackToken);
+        public async Task PostTalkToChannel(Conference conference, Talk talk, string talkUrl, string thumbnailUrl)
+        {
+            var connection = await connector.Connect(config.SlackToken);
             if (connection == null)
-			{
-				// TODO: Try again etc
-				return;
-			}
+            {
+                // TODO: Try again etc
+                return;
+            }
             
-			var channelId = config.UseTestSlackChannel ? TestSlackChannel : conference.SlackChannelId;
+            var channelId = config.UseTestSlackChannel ? TestSlackChannel : conference.SlackChannelId;
             var channel = new SlackChatHub { Id = channelId };
-			var message = new BotMessage
-			{
-				ChatHub = channel,
-				Attachments = new[] {
-					new SlackAttachment {
-						Title = talk.TalkName,
-						TitleLink = talkUrl,
-						Text = talk.Description,
-						Fallback = talk.Description,
-						AuthorName = talk.Speaker,
-						ImageUrl = thumbnailUrl
+            var message = new BotMessage
+            {
+                ChatHub = channel,
+                Attachments = new[] {
+                    new SlackAttachment {
+                        Title = talk.TalkName,
+                        TitleLink = talkUrl,
+                        Text = talk.Description,
+                        Fallback = talk.Description,                  
+                        AuthorName = talk.Speaker,
+						ColorHex = "#36a64f",
+						ThumbUrl = thumbnailUrl
                     }
                 }
             };
@@ -49,6 +50,6 @@ namespace MediaServer.Services
             await connection.Say(message);
 
             await connection.Close();
-		}
+        }
     }
 }
