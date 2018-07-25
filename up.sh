d544e3c028115e732ce4f39f11780a2e5b068c99
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2018-07-25 13:21:50 +0200

    CI-test

--- up.sh
+++ up.sh
@@ -8,4 +8,4 @@ export tag=:$(cat MediaServer/wwwroot/VERSION.txt)
 export populate_tag=:$(cat SideCars/CacheWarmer/VERSION.txt)
 export slack_tag=:$(cat SideCars/SlackIntegration/VERSION.txt)
 
-docker-compose up -d
+docker-compose up -d 
